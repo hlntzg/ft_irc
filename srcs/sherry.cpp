410eff15b0ad0d9f976d088989e4fd35ea1c0a2a
Jingjing Wu <sherry.helenius@gmail.com>
2025-05-12 09:37:44 +0300

    privmsg

--- srcs/sherry.cpp
+++ srcs/sherry.cpp
@@ -1,9 +1,22 @@
 #include "Server.hpp"
 
-// void	Server::privmsgCommand(Message& msg, Client& cli){
-// 	std::vector<std::string>	channels = msg.getChannelList();
-// 	//??????
-// }
+/**
+ * @brief User can use this PRIVMSG to send a message to the whole channel users
+ * or send it to single user
+ *
+ * Syntax: (and the limisted of the arguments except tailing message is 4)
+ *    PRIVMSG #42Helsinki :Hello everyone!
+ *    PRIVMSG john_doe :Hey, how are you?
+ */
+void	Server::privmsgCommand(Message& msg, Client& cli){
+	std::vector<std::string>	channels = msg.getChannels();
+	std::vector<std::string>	users = msg.getUsers();
+	if (channels.size() + users.size() > 4){
+		responseToClient(cli, tooManyTargets(cli.getNick()));
+		return;
+	}
+
+}
 
 /**
  * @brief Join a user into a channel
@@ -14,45 +27,50 @@
  *            Create a new channel, set the user as the operator.Notify the user
  *            who has joined
  *      3.2 If exist :
- * 	        -- Checking if the channel is full. If yes, can't join(ERR_CHANNELISFULL).
+ * 	        -- Checkingif the user exists in the channel already. If yes, then do nothing
+ *          -- Checking if the channel is full. If yes, can't join(ERR_CHANNELISFULL).
  *          -- Checking if the channel is invite_only. If yes, checking if the user
  *              on the invitee list. If no, can't join(ERR_INVITEONLYCHAN)
  * 			-- Checking if the channel needs a key. If yes, checking if the user provide
  * 	           the matched key.
+ * Command syntax:
+ * 		JOIN #channel1 [<#channel2> <#channel3> <#channel4>]
  *
  */
-// void	Server::joinCommand(Message& msg, Client& cli){
-// 	std::vector<std::string>	channels = msg.getChannelList();
-// 	if (channels.size() == 0){
-// 		responseToClient(cli, needMoreParams("JOIN"));
-// 		return;
-// 	} else if (channels.size() > TARGET_LIM_IN_ONE_CMD){
-// 		responseToClient(cli, tooManyTargets(cli.getNick()));
-// 		return;
-// 	}
-// 	for (const auto& chan_name : channels){
-// 		std::shared_ptr<Channel> channel = getChannelByName(chan_name);
-// 		if (channel == nullptr){
-// 			Channel new_channel(chan_name, cli);
-// 			responseToClient(cli, rplJoinChannel(cli, chan_name));
-// 		} else {
-// 			const std::unordered_set<Client*>& user_list = channel->getChannelUsers();
-// 			channel->addNewUser(cli);
-// 			for (const auto& user : user_list){
-// 				responseToClient(user, )
-// 			}
-// 		}
-// 	}
-// }
+// continue when we decide how to get the users list/invite list....
+void	Server::joinCommand(Message& msg, Client& cli){
+	const std::string&	nick = cli.getNick();
+	std::vector<std::string>	channels = msg.getChannels();
+	// checking if the arguments number is valid
+	if (channels.size() == 0){
+		responseToClient(cli, needMoreParams("JOIN"));
+		return;
+	} else if (channels.size() > TARGET_LIM_IN_ONE_CMD){
+		responseToClient(cli, tooManyTargets(nick));
+		return;
+	}
+	for (const auto& chan_name : channels){
+		std::shared_ptr<Channel> channel = getChannelByName(chan_name);
+		if (channel == nullptr){
+			Channel new_channel(chan_name, cli);
+			responseToClient(cli, rplJoinChannel(cli, chan_name));
+		} else {
+			if (channel->addNewUser(cli) == true){
+				
+			}
 
-// bool	Server::isExistedChannel(const std::string& channel_name){
-// 	for (const auto& [name, channelPtr] : channels_){
-// 		if (name == channel_name){
-// 			return true;
-// 		}
-// 	}
-// 	return false;
-// }
+		}
+	}
+}
+
+bool	Server::isExistedChannel(const std::string& channel_name){
+	for (const auto& [name, channelPtr] : channels_){
+		if (name == channel_name){
+			return true;
+		}
+	}
+	return false;
+}
 
 
 // {PART, &Server::partCommand},
